5fa54b00898e832e7fb3da4c247859644d968771
Marek Melichar <marek.melichar@icloud.com>
2020-08-27 08:02:02 +0200

    barcode api btn

--- src/app/components/Home/Home.jsx
+++ src/app/components/Home/Home.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import { withTranslation } from 'react-i18next'
+import React from 'react';
+import { withTranslation } from 'react-i18next';
+import axios from 'axios';
 
 const Home = ({ t }) => {
+  const getRowData = async () => {
+    const res = await axios.get(
+      'https://inventory.ixperta.net/api/barcode/4220072421',
+      {
+        // Axios looks for the `auth` option, and, if it is set, formats a
+        // basic auth header for you automatically.
+        auth: {
+          username: 'Ixperta',
+          password: 'Ixperta*1',
+        },
+      }
+    );
+
+    console.log('res', res);
+  };
   return (
     <div>
       <h1>{t('homepage')}</h1>
+      <button onClick={getRowData}>CLICK ME</button>
     </div>
-  )
-}
+  );
+};
 
-export default withTranslation()(Home)
+export default withTranslation()(Home);
